6a5641068da53d403e4e9d9f9b82885d0df51f68
tomisq <tomask1298@gmail.com>
2024-09-27 13:18:08 +0200

    nevim

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        HistorickeVozidlo auto = new HistorickeVozidlo("Audi", 1942,
+                new BigDecimal("13500"), false, new Vlastnik());
+        auto.setRokVyroby(1938);
+        System.out.println(auto.getRokVyroby());
+
+        ArrayList <String> seznamJizd = new ArrayList<>();
+        ArrayList <String> seznamJizd1 = new ArrayList<>();
+        seznamJizd.add("Audi");
+        seznamJizd.add("BMW");
+        seznamJizd.add("Mazda");
+        seznamJizd1.add("Audi");
+        seznamJizd1.add("BMW");
+        seznamJizd1.add("Mazda");
+
+        public void vratSeznam; {
+
+        }
+   }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Vlastnik.java
@@ -0,0 +1,7 @@
+import java.time.LocalDate;
+
+public class Vlastnik {
+    private String jmeno;
+    private LocalDate datumNarozeni;
+
+}
